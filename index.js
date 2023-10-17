e65e425786ee4b9ec2ee7d1d28d5e60f6b652f1b
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-10-18 03:50:52 +0600

    create get/read oparetion for brand

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const brandCollection = client.db("brandDB").collection("brand");
+    app.get("/brand",async(req, res)=> {
+        const result = await brandCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
