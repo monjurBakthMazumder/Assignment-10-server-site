9a4c1d1408302cb0da15b3a6b5ecab0198458c83
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-10-18 18:39:48 +0600

    create get api for single product

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -40,6 +40,19 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/products/:brand', async(req,res)=> {
+      const brand = req.params.brand
+      const filter = {brand: brand}
+      const result = await productCollection.find(filter).toArray()
+      res.send(result)
+    })
+
+    app.get('/product/:id', async(req,res)=> {
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const result = await productCollection.find(filter).toArray()
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
