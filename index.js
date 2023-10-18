a3103a85c3bd0d1e4b73cdcbfe4c762c9bf32cda
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-10-18 22:32:20 +0600

    create put api for update page

--- index.js
+++ index.js
@@ -54,6 +54,26 @@ async function run() {
       res.send(result)
     })
 
+    app.put('/product/:id', async(req,res)=> {
+      const id = req.params.id
+      const filter = {_id : new ObjectId(id)}
+      const product = req.body
+      const options = { upsert: true };
+      const updateProduct = {
+        $set: {
+          name: product.name,
+          brand: product.brand,
+          price: product.price,
+          type: product.type,
+          rating: product.rating,
+          description: product.description,
+          img: product.img,
+        }
+      }
+      const result = await productCollection.updateOne(filter, updateProduct, options)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
